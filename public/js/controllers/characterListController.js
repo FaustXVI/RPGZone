e4f7a06b810a8557176c63b6d76e0c4a85703c96
FaustXVI <xavier.detant@gmail.com>
2014-02-23 15:45:06 +0100

    Add modal for character deletion confirmation

--- public/js/controllers/characterListController.js
+++ public/js/controllers/characterListController.js
@@ -1,18 +1,40 @@
-var characterListController = angular.module('characterListController', ['characterService']);
+var characterListController = angular.module('characterListController', ['characterService', 'ui.bootstrap']);
 
-characterListController.controller('characterListController', function ($scope, characterService) {
+characterListController.controller('characterListController', function ($scope, characterService, $modal) {
 
     function loadCharacters() {
         $scope.characters = characterService.query();
     }
 
-    $scope.suppress = function (id) {
-        console.log("Suppress " + id);
-        characterService.delete({characterId: id}, function () {
-                loadCharacters();
+    $scope.suppress = function (character) {
+        var modalInstance = $modal.open({
+            templateUrl: "modals/confirmDeleteFromList.html",
+            controller: ModalInstanceCtrl,
+            resolve: {
+                item: function () {
+                    return character.id;
+                }
             }
-        );
+        });
+        modalInstance.result.then(function () {
+            characterService.delete({characterId: character._id}, function () {
+                    loadCharacters();
+                }
+            );
+        });
+    };
 
+    var ModalInstanceCtrl = function ($scope, $modalInstance, item) {
+
+        $scope.item = item;
+
+        $scope.ok = function () {
+            $modalInstance.close(item);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
     };
 
     loadCharacters();
