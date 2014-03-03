db1c218408334e7f120fe6c38ab7d0b8e67ae290
FaustXVI <xavier.detant@gmail.com>
2014-03-04 00:06:37 +0100

    Simpler calculations

--- public/js/utils/calculator.js
+++ public/js/utils/calculator.js
@@ -26,51 +26,54 @@ calculator.factory('calculator', function () {
                 return 0;
             }
         },
-        calculateHalfLevel: function (level) {
+        calculateHalfLevel: function () {
+            var level = this.character.id.level;
             if (level) {
                 return Math.floor(level / 2);
             } else {
                 return 0;
             }
         },
-        calculateTotalMod: function (value, level) {
+        calculateTotalMod: function (value) {
             if (level) {
-                return this.calculateMod(value) + this.calculateHalfLevel(level);
+                return this.calculateMod(value) + this.calculateHalfLevel();
             } else {
                 return 0;
             }
         },
-        calculateBloodied: function (maxHp) {
-            if (maxHp) {
-                return Math.floor(maxHp / 2);
+        calculateBloodied: function () {
+            var max = this.character.hp.max;
+            if (max) {
+                return Math.floor(max / 2);
             } else {
                 return 0;
             }
         },
-        calculateSurge: function (maxHp) {
-            if (maxHp) {
-                return Math.floor(maxHp / 4);
+        calculateSurge: function () {
+            var max = this.character.hp.max;
+            if (max) {
+                return Math.floor(max / 4);
             } else {
                 return 0;
             }
         },
-        calculateSumMod: function (character, abilities) {
+        calculateSumMod: function (abilities) {
             if (abilities) {
                 var sum = 0;
                 for (var i = 0; i < abilities.length; i++) {
                     var abi = abilities[i];
-                    sum += this.calculateMod(character.ability[abi]);
+                    sum += this.calculateMod(this.character.ability[abi]);
                 }
                 return sum;
             } else {
                 return 0;
             }
         },
-        calculateSkill: function (character, abi, skill, level) {
-            if (skill && character.ability) {
-                return this.calculateTotalMod(character.ability[abi], level)
-                    + this.valueOrZero(character.skills[skill].trained)
-                    + this.valueOrZero(character.skills[skill].bonus);
+        calculateSkill: function (abi, skill) {
+            if (skill && this.character.ability) {
+                return this.calculateTotalMod(this.character.ability[abi])
+                    + this.valueOrZero(this.character.skills[skill].trained)
+                    + this.valueOrZero(this.character.skills[skill].bonus);
             } else {
                 return 0;
             }
